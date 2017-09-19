ac0ce2b81d2ded97712e1feee46e32655743ee31
Johan Van de Wauw <johan@gisky.be>
2017-09-19 14:44:27 +0200

    flake-8 reported problems fixed

--- niche_vlaanderen/acidity.py
+++ niche_vlaanderen/acidity.py
@@ -7,18 +7,18 @@ import pandas as pd
 class Acidity(object):
     '''
     '''
-    def __init__(self, ct_acidity = resource_filename(
+    def __init__(self, ct_acidity=resource_filename(
             "niche_vlaanderen", "../SystemTables/acidity.csv"),
-            ct_soil_mlw_class = resource_filename(
+            ct_soil_mlw_class=resource_filename(
             "niche_vlaanderen", "../SystemTables/soil_mlw_class.csv"),
-            ct_soil_codes = resource_filename(
-            "niche_vlaanderen", "../SystemTables/soil_codes.csv")
-        ):
+            ct_soil_codes=resource_filename(
+            "niche_vlaanderen", "../SystemTables/soil_codes.csv")):
+
         self._ct_acidity = pd.read_csv(ct_acidity)
-        self._ct_soil_mlw_class = pd.read_csv(ct_soil_mlw_class)
+        self._ct_soil_mlw = pd.read_csv(ct_soil_mlw_class)
         self._ct_soil_codes = pd.read_csv(ct_soil_codes).set_index("soil_code")
 
-    def _get_soil_mlw (self, soil_code, mlw):
+    def _get_soil_mlw(self, soil_code, mlw):
         # determine soil_group for soil_code
         orig_shape = mlw.shape
         soil_code = soil_code.flatten()
@@ -27,12 +27,16 @@ class Acidity(object):
         soil_group = np.array(self._ct_soil_codes.soil_group[soil_code])
 
         result = np.full(soil_code.shape, -99)
-        for selected_soil_group, table in self._ct_soil_mlw_class.groupby(["soil_group"]):
+        for sel_soil_group, table in self._ct_soil_mlw.groupby(["soil_group"]):
             table_sel = table.copy().reset_index(drop=True)
             index = np.digitize(mlw, table_sel.mlw_max, right=True)
-            selection = (soil_group == selected_soil_group)
+            selection = (soil_group == sel_soil_group)
             result[selection] = table_sel.soil_mlw_class[index][selection]
-        
+
+        result.reshape(orig_shape)
         return result
 
-        
+    def _get_mineral_richness_class(self, conductivity):
+        reclass = (conductivity >= 500).astype("int8")
+        reclass[np.isnan(conductivity)] = -99
+        return reclass
