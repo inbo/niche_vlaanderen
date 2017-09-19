a2cba9ffcaf0958f9724d5c0f069c973be2ecfad
Johan Van de Wauw <johan@gisky.be>
2017-09-19 12:26:29 +0200

    Acidity - add soil_mlw calculation

---
+++ niche_vlaanderen/acidity.py
@@ -0,0 +1,38 @@
+from pkg_resources import resource_filename
+
+import numpy as np
+import pandas as pd
+
+
+class Acidity(object):
+    '''
+    '''
+    def __init__(self, ct_acidity = resource_filename(
+            "niche_vlaanderen", "../SystemTables/acidity.csv"),
+            ct_soil_mlw_class = resource_filename(
+            "niche_vlaanderen", "../SystemTables/soil_mlw_class.csv"),
+            ct_soil_codes = resource_filename(
+            "niche_vlaanderen", "../SystemTables/soil_codes.csv")
+        ):
+        self._ct_acidity = pd.read_csv(ct_acidity)
+        self._ct_soil_mlw_class = pd.read_csv(ct_soil_mlw_class)
+        self._ct_soil_codes = pd.read_csv(ct_soil_codes).set_index("soil_code")
+
+    def _get_soil_mlw (self, soil_code, mlw):
+        # determine soil_group for soil_code
+        orig_shape = mlw.shape
+        soil_code = soil_code.flatten()
+        mlw = mlw.flatten()
+
+        soil_group = np.array(self._ct_soil_codes.soil_group[soil_code])
+
+        result = np.full(soil_code.shape, -99)
+        for selected_soil_group, table in self._ct_soil_mlw_class.groupby(["soil_group"]):
+            table_sel = table.copy().reset_index(drop=True)
+            index = np.digitize(mlw, table_sel.mlw_max, right=True)
+            selection = (soil_group == selected_soil_group)
+            result[selection] = table_sel.soil_mlw_class[index][selection]
+        
+        return result
+
+        
---
+++ tests/test_acidity.py
@@ -0,0 +1,18 @@
+
+from unittest import TestCase
+
+import numpy as np
+import gdal
+import osr
+
+import niche_vlaanderen
+
+class testAcidity(TestCase):
+
+    def test_get_soil_mlw(self):
+        mlw = np.array([50,66])
+        soil_codes = np.array([140000, 40000])
+        a = niche_vlaanderen.Acidity()
+        result = a._get_soil_mlw(soil_codes, mlw)
+
+        np.testing.assert_equal(np.array([1, 9]), result)
