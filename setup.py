7640675024ecea550c253eba29ba59c2645fc509
Johan Van de Wauw <johan@gisky.be>
2017-09-20 18:50:11 +0200

    Add enum34 dependency for older packages

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+requirements = [
+        'pandas',
+        'numpy',
+        ]
+
+if sys.version_info < (3, 4):
+    requirements.append('enum34')
+
 setup(name='niche_vlaanderen',
       version="0.0.1",
       description='NICHE Vlaanderen',
@@ -7,10 +15,7 @@ setup(name='niche_vlaanderen',
       author='Johan Van de Wauw',
       author_email='johan.vandewauw@inbo.be',
       license='MIT',
-      install_requires=[
-          'pandas',
-          'numpy'
-      ],
+      install_requires=requirements,
       classifiers=[
           'Development Status :: 1 - Planning',
           'Inteded Audience :: Science/Research',
