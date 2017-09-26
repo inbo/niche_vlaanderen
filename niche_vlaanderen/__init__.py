0db061806950447f99028b9ac20d5e60a4da28f4
Johan Van de Wauw <johan@gisky.be>
2017-09-26 11:23:47 +0200

    Remove obsolete input_types

--- niche_vlaanderen/__init__.py
+++ niche_vlaanderen/__init__.py
@@ -1,6 +1,5 @@
 from .acidity import Acidity
 from .niche import Niche
 from .nutrient_level import NutrientLevel
-from .input_types import InputLayerTypes
 from .vegetation import Vegetation
 from .version import __version__
