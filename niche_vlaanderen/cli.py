82fd6126cc62ac5b84c4fe38f6922987d3a72ea3
Johan Van de Wauw <johan@gisky.be>
2017-11-06 09:58:02 +0100

    Add --config argument to cli

--- niche_vlaanderen/cli.py
+++ niche_vlaanderen/cli.py
@@ -3,8 +3,12 @@ import niche_vlaanderen
 
 
 @click.command()
+@click.option('--example', is_flag=True,
+              help='prints an example configuration file')
 @click.argument('config')
 def cli(config):
+    """Command line interface to the NICHE vegetation model
+    """
     n = niche_vlaanderen.Niche()
     n.run_config_file(config)
-    print(n)
+    click.echo(n)
