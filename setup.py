443a66e04c18de2653791710b387220bf456380d
stijnvanhoey <stijnvanhoey@gmail.com>
2017-11-13 21:55:49 +0100

    Fix bug on history file

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ from setuptools import setup, find_packages
 
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+    readme = readme_file.read()s
 
 requirements = [
         'pandas',
