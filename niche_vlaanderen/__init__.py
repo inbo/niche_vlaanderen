b57495b4b9581bef08f7018e2dd561dac42369d6
Johan Van de Wauw <johan@gisky.be>
2017-11-19 10:18:35 +0100

    Add NicheDelta to namespace

--- niche_vlaanderen/__init__.py
+++ niche_vlaanderen/__init__.py
@@ -1,5 +1,5 @@
 from .acidity import Acidity # noqa
-from .niche import Niche # noqa
+from .niche import Niche, NicheDelta # noqa
 from .nutrient_level import NutrientLevel # noqa
 from .vegetation import Vegetation # noqa
 from .version import __version__ # noqa
