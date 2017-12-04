de66fe28d2bd3e118a468257601d2bdfcc4341ed
Johan Van de Wauw <johan@gisky.be>
2017-12-04 16:01:05 +0100

    Add FloodPlain class to module namespace

--- niche_vlaanderen/__init__.py
+++ niche_vlaanderen/__init__.py
@@ -3,3 +3,4 @@ from .niche import Niche, NicheDelta # noqa
 from .nutrient_level import NutrientLevel # noqa
 from .vegetation import Vegetation # noqa
 from .version import __version__ # noqa
+from .floodplain import FloodPlain
