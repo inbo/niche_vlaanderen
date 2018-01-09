d212840f76a426c5dc08d1030071dc3f299885fa
Johan Van de Wauw <johan@gisky.be>
2018-01-09 12:43:38 +0100

    Specify correct page size for latex docs

--- docs/conf.py
+++ docs/conf.py
@@ -256,7 +256,7 @@ htmlhelp_basename = 'niche_vlaanderendoc'
 latex_elements = {
      # The paper size ('letterpaper' or 'a4paper').
      #
-     # 'papersize': 'letterpaper',
+     'papersize': 'a4paper',
 
      # The font size ('10pt', '11pt' or '12pt').
      #
