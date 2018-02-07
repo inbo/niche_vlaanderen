68530b85e25388c200b408c00e05ed08978426ce
Johan Van de Wauw <johan@gisky.be>
2018-02-07 14:33:37 +0100

    Fix cli test on windows

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import niche_vlaanderen.cli as nv_cli
 import os
 
 def test_cli_no_param():
-    expected = "No config file added. Use --help for more info" + os.linesep
+    expected = "No config file added. Use --help for more info\n"
     runner = CliRunner()
     result = runner.invoke(nv_cli.cli)
     assert result.output == expected
