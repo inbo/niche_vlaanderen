6829e8baef77841b8006cd8d5930133bc3bc5dc8
Johan Van de Wauw <johan@gisky.be>
2018-02-08 15:38:53 +0100

    Add cli test for floodplain using overwritten codetable

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,3 +7,19 @@ def test_cli_no_param():
     runner = CliRunner()
     result = runner.invoke(nv_cli.cli)
     assert result.output == expected
+
+def test_cli_floodplain():
+    runner = CliRunner()
+
+    result = runner.invoke(nv_cli.cli, ['tests/floodplain-codetables.yml'])
+
+    expected_files = [
+        'D1.tif', 'D2.tif', 'D3.tif', 'D4.tif', 'D5.tif', 'D6.tif',
+        'D7.tif', 'D8.tif', 'D9.tif', 'D10.tif', 'D11.tif', 'D12.tif',
+        'D13.tif', 'D14.tif', 'D15.tif', 'D16.tif', 'D17.tif', 'D18.tif',
+        'D19.tif', 'D20.tif', 'D21.tif', 'D22.tif', 'D23.tif', 'D24.tif',
+        'D25.tif', 'D26.tif', 'D27.tif', 'D28.tif']
+
+    dir = os.listdir("_output")
+    result_eerste = [f.startswith("eerste") for f in dir]
+    assert sum(result_eerste) == 24
