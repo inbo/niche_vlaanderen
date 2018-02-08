758995b9a5103dc8f81cc78647f5799315ebf891
Johan Van de Wauw <johan@gisky.be>
2018-02-08 16:30:09 +0100

    Skip test on py27 + rasterio from conda

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,12 +2,15 @@ from click.testing import CliRunner
 import niche_vlaanderen.cli as nv_cli
 import os
 
+
 def test_cli_no_param():
     expected = "No config file added. Use --help for more info\n"
     runner = CliRunner()
     result = runner.invoke(nv_cli.cli)
     assert result.output == expected
 
+
+@pytest.mark.skipwindows27
 def test_cli_floodplain():
     runner = CliRunner()
 
