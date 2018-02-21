72b8e0bd6fae6c34742c95256d732d157df6a92a
Johan Van de Wauw <johan@gisky.be>
2018-02-21 14:00:29 +0100

    Fix tests floodplains config file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,13 +17,6 @@ def test_cli_floodplain():
 
     result = runner.invoke(nv_cli.cli, ['tests/floodplain-codetables.yml'])
 
-    expected_files = [
-        'D1.tif', 'D2.tif', 'D3.tif', 'D4.tif', 'D5.tif', 'D6.tif',
-        'D7.tif', 'D8.tif', 'D9.tif', 'D10.tif', 'D11.tif', 'D12.tif',
-        'D13.tif', 'D14.tif', 'D15.tif', 'D16.tif', 'D17.tif', 'D18.tif',
-        'D19.tif', 'D20.tif', 'D21.tif', 'D22.tif', 'D23.tif', 'D24.tif',
-        'D25.tif', 'D26.tif', 'D27.tif', 'D28.tif']
-
     dir = os.listdir("_output")
     result_eerste = [f.startswith("eerste") for f in dir]
     assert sum(result_eerste) == 24
