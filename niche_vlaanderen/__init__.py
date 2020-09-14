4554f350fd64a219b4514afd94a05092d387360e
Johan Van de Wauw <johan@gisky.be>
2020-09-14 13:01:28 +0200

    Limit namespace to base classes/functions
    Closes #55
    
    Note that ipython usually does not respect these options, so end-users
    will probably not notice much of a difference.
    
    https://github.com/ipython/ipykernel/issues/129

--- niche_vlaanderen/__init__.py
+++ niche_vlaanderen/__init__.py
@@ -1,6 +1,8 @@
-from .acidity import Acidity # noqa
 from .niche import Niche, NicheDelta, conductivity2minerality # noqa
+from .acidity import Acidity # noqa
 from .nutrient_level import NutrientLevel # noqa
 from .vegetation import Vegetation # noqa
 from .version import __version__ # noqa
 from .flooding import Flooding # noqa
+
+__all__ = ['Acidity', 'Niche', 'NicheDelta', 'conductivity2minerality', 'NutrientLevel', 'Vegetation', 'Flooding']
