2fdab909f24f84512a7e154f0fd20ca410e360ad
Johan Van de Wauw <johan@gisky.be>
2023-02-27 13:36:29 +0100

    Add test for multiple mappings

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -12,12 +12,13 @@ from niche_vlaanderen.validation import NicheValidation, NicheValidationExceptio
 def test_validation(zwarte_beek_niche):
     zwarte_beek_niche.run()
 
-    no = NicheValidation(niche=zwarte_beek_niche, map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp")
+    no = NicheValidation(
+        niche=zwarte_beek_niche,
+        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp",
+    )
 
     # get the index of a record we know should have a nich_1_1 value
-    test_index = (
-        no.map.reset_index().set_index("OBJECTID").loc[[37]]["index"].values[0]
-    )
+    test_index = no.map.reset_index().set_index("OBJECTID").loc[[37]]["index"].values[0]
     assert test_index == 4
     item = no.map.iloc[test_index]
     print(item)
@@ -35,34 +36,80 @@ def test_validation(zwarte_beek_niche):
     assert "map: tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp" in str(no)
     assert "niche object: zwarte beek" in str(no)
 
+
 def test_validation_custom_vegetation(zwarte_beek_niche):
     zwarte_beek_niche.run()
 
-    no = NicheValidation(niche=zwarte_beek_niche, map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp", mapping_file="tests/data/hab_niche_test.csv")
+    no = NicheValidation(
+        niche=zwarte_beek_niche,
+        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp",
+        mapping_file="tests/data/hab_niche_test.csv",
+    )
 
     # 5 hab columns * 3 potential values
     # three niche types for all 2 HAB types
     assert len(no.map.columns[no.map.columns.str.startswith("NICH")]) == 15
+
+
+def test_validation_multiple_mapping(tmpdir, zwarte_beek_niche):
+    """
+    Test that area's are correct if multiple vegetation types map to the same niche type
+    """
+    zwarte_beek_niche.run()
+    mapping = pd.read_csv("tests/data/hab_niche_test.csv")
+    mapping["NICHE"] = 4
+    mapping.drop_duplicates(inplace=True, ignore_index=True)
+    print(mapping)
+    mapping.to_csv(tmpdir / "mapping.csv")
+    no = NicheValidation(
+        niche=zwarte_beek_niche,
+        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp",
+        mapping_file=tmpdir / "mapping.csv",
+    )
+
+    np.testing.assert_almost_equal(
+        no.potential_presence.sum()["area_ha"].to_numpy(),
+        (no.area_pot.sum() + no.area_nonpot.sum()).to_numpy(),
+    )
+
 
 def test_validation_invalid_niche():
     nv = 6
     with pytest.raises(ValueError):
         NicheValidation(niche=nv, map="tests/data/bwk_fake/bwk_fake_extentok.shp")
 
+
 def test_validation_warning_overlap(zwarte_beek_niche):
     zwarte_beek_niche.run()
     with pytest.warns(UserWarning):
-        NicheValidation(niche=zwarte_beek_niche,
-                        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek.shp",
-                        upscale=1)
+        NicheValidation(
+            niche=zwarte_beek_niche,
+            map="tests/data/bwk/BWK_2020_clip_ZwarteBeek.shp",
+            upscale=1,
+        )
+
 
 def test_validation_write(tmpdir, zwarte_beek_niche):
     zwarte_beek_niche.run()
-    validation = NicheValidation(niche=zwarte_beek_niche, map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp")
+    validation = NicheValidation(
+        niche=zwarte_beek_niche,
+        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp",
+    )
     validation.write(tmpdir)
     files_written = os.listdir(tmpdir)
-    expected_files = {'area_nonpot_optimistic.csv', 'area_pot_perc.csv', 'potential_presence.csv', 'area_pot.csv', 'overlay.gpkg', 'area_effective.csv', 'summary.csv', 'veg_present.csv', 'area_pot_perc_optimistic.csv', 'area_nonpot.csv'}
-    assert set(files_written)== expected_files
+    expected_files = {
+        "area_nonpot_optimistic.csv",
+        "area_pot_perc.csv",
+        "potential_presence.csv",
+        "area_pot.csv",
+        "overlay.gpkg",
+        "area_effective.csv",
+        "summary.csv",
+        "veg_present.csv",
+        "area_pot_perc_optimistic.csv",
+        "area_nonpot.csv",
+    }
+    assert set(files_written) == expected_files
 
     # should raise because the dir eists and is not empty
     with pytest.raises(NicheValidationException):
@@ -70,10 +117,11 @@ def test_validation_write(tmpdir, zwarte_beek_niche):
 
     # raises because the path exists and is not a folder
     with pytest.raises(NicheValidationException):
-        validation.write(tmpdir/"overlay.gpkg")
+        validation.write(tmpdir / "overlay.gpkg")
 
     # should not raise
     validation.write(tmpdir, overwrite_files=True)
+
 
 def test_validation_write_customid(tmpdir):
     """Test writing using a custom id"""
@@ -87,11 +135,22 @@ def test_validation_write_customid(tmpdir):
     validation = NicheValidation(niche=nv, map=str(tmpdir / "sel_id.shp"), id="new_id")
     validation.write(tmpdir / "validation")
     files_written = os.listdir(tmpdir / "validation")
-    expected_files = {'area_nonpot_optimistic.csv', 'area_pot_perc.csv', 'potential_presence.csv', 'area_pot.csv', 'overlay.gpkg', 'area_effective.csv', 'summary.csv', 'veg_present.csv', 'area_pot_perc_optimistic.csv', 'area_nonpot.csv'}
-    assert set(files_written)== expected_files
+    expected_files = {
+        "area_nonpot_optimistic.csv",
+        "area_pot_perc.csv",
+        "potential_presence.csv",
+        "area_pot.csv",
+        "overlay.gpkg",
+        "area_effective.csv",
+        "summary.csv",
+        "veg_present.csv",
+        "area_pot_perc_optimistic.csv",
+        "area_nonpot.csv",
+    }
+    assert set(files_written) == expected_files
 
-    overlay = gpd.read_file(str(tmpdir /"validation"/ "overlay.gpkg"))
-    area = pd.read_csv(tmpdir / "validation" /"area_pot_perc.csv")
+    overlay = gpd.read_file(str(tmpdir / "validation" / "overlay.gpkg"))
+    area = pd.read_csv(tmpdir / "validation" / "area_pot_perc.csv")
 
     assert "new_id" in overlay.columns
     assert "new_id" in area.columns
@@ -104,11 +163,15 @@ def test_validation_multiple_veg(zwarte_beek_niche):
     """
     zwarte_beek_niche.run()
 
-    no_simplebr_fkok = NicheValidation(niche=zwarte_beek_niche, map= "tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp", id="OBJECTID") # see zip attached
+    no_simplebr_fkok = NicheValidation(
+        niche=zwarte_beek_niche,
+        map="tests/data/bwk/BWK_2020_clip_ZwarteBeek_simplified.shp",
+        id="OBJECTID",
+    )  # see zip attached
     print(no_simplebr_fkok.area_effective)
     print(no_simplebr_fkok.summary)
     # effective area should be 80% of the shape (40+40)
-    assert pytest.approx(no_simplebr_fkok.area_effective.loc[37][14]) ==0.89288
+    assert pytest.approx(no_simplebr_fkok.area_effective.loc[37][14]) == 0.89288
 
 
 def test_validation_hablegend(tmpdir):
