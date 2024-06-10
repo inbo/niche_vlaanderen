b7527c698caa5ff237cf8c7305be9d2fcc3ae226
davyd <davy.depreiter@fluves.com>
2024-06-10 13:40:35 +0200

    Change the colorscale range to -200 -> 0 instead of 0 -> 200. Invert the color scale colors so that the color of the zero value is as previous.

--- niche_vlaanderen/niche.py
+++ niche_vlaanderen/niche.py
@@ -818,7 +818,7 @@ class Niche(object):
                 norm = Normalize(-50, 50)
 
         if key in {"mhw", "mlw", "msw"} and fixed_scale:
-            norm = Normalize(200, 0)
+            norm = Normalize(-200, 0)
 
         if v is None:
             raise NicheException("Invalid key specified")
@@ -846,6 +846,7 @@ class Niche(object):
                 handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0
             )
         else:
+            im.set_cmap(im.get_cmap().reversed())
             plt.colorbar()
 
         return ax
