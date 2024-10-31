3fe72f402f4ae8d40a04accb1ea810e9f39d0086
Stijn Van Hoey <stijn@fluves.com>
2024-10-31 13:04:42 +0100

    Bump version

--- niche_vlaanderen/version.py
+++ niche_vlaanderen/version.py
@@ -1,4 +1,4 @@
-__version__ = "2.0"
+__version__ = "2.1"
 __reference_table_version__ = "12C"
 __reference_table_source__ = "10.5281/zenodo.10521548"
 __reference_table_file__ = "NICHE_FL_referencegroundwaterlevels_v12C.csv"
